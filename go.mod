875b95a5d76f0e6e7a3230d86c3c2fc86315d8a4
Matt Huang <r06245017@ntu.edu.tw>
2023-05-17 17:43:18 +0800

    modified file

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module filesize
 
-go 1.19
+go 1.18
--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 		if err != nil {
 			return err
 		}
-		if info.IsDir() && (info.Name() == "$RECYCLE.BIN" || info.Name() == "Config.Msi" || info.Name() == "Recovery") {
+		if info.IsDir() && (info.Name() == "$RECYCLE.BIN" || info.Name() == "Config.Msi" || info.Name() == "Recovery" || info.Name() == "System Volume Information") {
 			return filepath.SkipDir
 		}
 		if info.IsDir() {
@@ -21,6 +21,14 @@ func main() {
 			if err == nil {
 				fmt.Println(path)
 				// 在這裡可以對已經被Git初始化的資料夾進行處理
+			} else if os.IsPermission(err) {
+				// 如果是權限不足的錯誤，就試著修改該資料夾的權限
+				err = os.Chmod(path, 0777)
+				if err != nil {
+					fmt.Printf("Failed to change permission for %s: %v\n", path, err)
+				}
+				// 然後跳過該資料夾，繼續尋找下一個資料夾
+				return filepath.SkipDir
 			} else if !os.IsNotExist(err) {
 				return err
 			}
